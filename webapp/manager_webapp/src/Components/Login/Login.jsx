7340d3cb9bb24c20355621be73f44483b4e0e251
Khaled Rezgui <khaledrezguipro@gmail.com>
2025-03-19 02:53:05 -0400

    Hiding forgot password so dmitry doesnt fck us

--- webapp/manager_webapp/src/Components/Login/Login.jsx
+++ webapp/manager_webapp/src/Components/Login/Login.jsx
@@ -81,10 +81,6 @@ const Login = () => {
           <CiLock className="icon" />
         </div>
 
-        <div className="forgot-password">
-          <Link to="/forgot-password">Forgot Password?</Link>
-        </div>
-
         <button
           type="submit"
           disabled={isSigningIn}
